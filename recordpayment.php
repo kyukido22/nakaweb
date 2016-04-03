4b854218cb7200e754b13b3fa859f1354b930c3e
john.cantin <john.cantin@gmail.com>
2016-04-03 15:11:48 -0500

    changed array contents check to key_exists
    Signed-off-by: john.cantin <john.cantin@gmail.com>

--- recordpayment.php
+++ recordpayment.php
@@ -2,8 +2,8 @@
 
 session_start();
 if (!isset($_SESSION["userid"])) {
-	header('Location: login.php');
-	exit ;
+    header('Location: login.php');
+    exit ;
 }
 
 $starttime = microtime(TRUE);
@@ -34,71 +34,72 @@ $cancontinue = TRUE;
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-	/*
-	 * record the payment
-	 */
+    /*
+     * record the payment
+     */
 
-	$dbconn = PDOconnect('nakaweb', $_SESSION["clientdefaults"]["host"], $logname);
-	foreach ($_SESSION['invoices'] as $key => $value) {
-		if ($_POST['checkno' . $value] != '') {
-			logit($logname, '  updataing:' . $value);
-			// create a pg conection
+    $dbconn = PDOconnect('nakaweb', $_SESSION["clientdefaults"]["host"], $logname);
+    foreach ($_SESSION['invoices'] as $key => $value) {
+        if (key_exists('checkno' . $value, $_POST)) {
+            logit($logname, '  updataing:' . $value);
+            // create a pg conection
 
-			try {
-				$theq = " update invoices";
-				$theq .= " set checknumber=:checknumber";
-				$theq .= " ,paymentreceived=now()";
-				$theq .= " ,receivedby=:receivedby";
-				$theq .= " where invoiceid=:invoiceid";
-				$pdoquery = $dbconn -> prepare($theq);
-				$pdoquery -> execute(array(//
-				':checknumber' => clean_user_input($_POST['checkno' . $value]), //
-				':receivedby' => $_SESSION["userid"], //
-				':invoiceid' => $value));
+            try {
+                $theq = " update invoices";
+                $theq .= " set checknumber=:checknumber";
+                $theq .= " ,paymentreceived=now()";
+                $theq .= " ,receivedby=:receivedby";
+                $theq .= " where invoiceid=:invoiceid";
+                $pdoquery = $dbconn -> prepare($theq);
+                $pdoquery -> execute(array(//
+                ':checknumber' => clean_user_input($_POST['checkno' . $value]),
+                // //
+                ':receivedby' => $_SESSION["userid"], //
+                ':invoiceid' => $value));
 
-			} catch (PDOException $e) {
-				logit($logname, '  **ERROR** on line ' . __LINE__ . ' with query - ' . $theq . ' ' . $e -> getMessage());
-				$results -> errortext = $e -> getMessage();
-				$cancontinue = FALSE;
-			}
-		}
-	}
+            } catch (PDOException $e) {
+                logit($logname, '  **ERROR** on line ' . __LINE__ . ' with query - ' . $theq . ' ' . $e -> getMessage());
+                $results -> errortext = $e -> getMessage();
+                $cancontinue = FALSE;
+            }
+        }
+    }
 
-	unset($_SESSION['invoices']);
-	logit($logname, 'GETting back to recordpayment');
-	header('Location: recordpayment.php');
-	exit ;
+    unset($_SESSION['invoices']);
+    logit($logname, 'GETting back to recordpayment');
+    header('Location: recordpayment.php');
+    exit ;
 
 } else {
 
-	// get invoices
-	$dbconnw = PDOconnect('nakaweb', $_SESSION["clientdefaults"]["host"], $logname);
-	$theq = " select fullname,invoiceid,invoicedate,paymentreceived,login,invoiceamount,";
-	$theq .= " case when login is null then ";
-	$theq .= '    \'<input type="text" name="checkno\'||invoiceid::text||\'">\'';
-	$theq .= " else checknumber end as checknumber";
-	$theq .= " from invoices i";
-	$theq .= " join client c on i.schoolid=c.clientid";
-	$theq .= " left join users u on u.userid=receivedby";
-	$theq .= " order by paymentreceived desc, invoicedate desc, invoiceamount desc";
-	try {
-		$pdoquery = $dbconnw -> prepare($theq);
-		$pdoquery -> setFetchMode(PDO::FETCH_OBJ);
-		$pdoquery -> execute();
-		$invoices = $pdoquery -> fetchAll();
+    // get invoices
+    $dbconnw = PDOconnect('nakaweb', $_SESSION["clientdefaults"]["host"], $logname);
+    $theq = " select fullname,invoiceid,invoicedate,paymentreceived,login,invoiceamount,";
+    $theq .= " case when login is null then ";
+    $theq .= '    \'<input type="text" name="checkno\'||invoiceid::text||\'">\'';
+    $theq .= " else checknumber end as checknumber";
+    $theq .= " from invoices i";
+    $theq .= " join client c on i.schoolid=c.clientid";
+    $theq .= " left join users u on u.userid=receivedby";
+    $theq .= " order by paymentreceived desc, invoicedate desc, invoiceamount desc";
+    try {
+        $pdoquery = $dbconnw -> prepare($theq);
+        $pdoquery -> setFetchMode(PDO::FETCH_OBJ);
+        $pdoquery -> execute();
+        $invoices = $pdoquery -> fetchAll();
 
-		unset($_SESSION['invoices']);
-		$i = 0;
-		foreach ($invoices as $key => $value) {
-			$_SESSION['invoices'][$i] = $value -> invoiceid;
-			$i++;
-		}
+        unset($_SESSION['invoices']);
+        $i = 0;
+        foreach ($invoices as $key => $value) {
+            $_SESSION['invoices'][$i] = $value -> invoiceid;
+            $i++;
+        }
 
-	} catch (PDOException $e) {
-		logit($logname, '  **ERROR** on line ' . __LINE__ . ' with query - ' . $theq . ' ' . $e -> getMessage());
-		$results -> errortext = $e -> getMessage();
-		$cancontinue = FALSE;
-	}
+    } catch (PDOException $e) {
+        logit($logname, '  **ERROR** on line ' . __LINE__ . ' with query - ' . $theq . ' ' . $e -> getMessage());
+        $results -> errortext = $e -> getMessage();
+        $cancontinue = FALSE;
+    }
 }
 
 $_SESSION['clientdefaults']['pagetitle'] = 'Record Invoice Payments';
